361b29f1ce50f4585adf4d3015baffdc52ad5369
Ahmad Alghooneh <a.alghooneh@gmail.com>
2025-11-01 15:05:01 -0400

    deployed web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="Personal-Portfolio">
         <Routes>
           <Route path="/" element={<Portfolio />} />
         </Routes>
